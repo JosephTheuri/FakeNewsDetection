66c6b703a0ddb66dbe484513493c15c774c70559
Joseph Theuri <jtheuri0@gmail.com>
2021-12-03 11:53:14 +0300

    alot of changes

--- src/preparation/import_data.py
+++ src/preparation/import_data.py
@@ -1,3 +1,4 @@
+import csv
 import pandas as pd
 
 class ImportData():
@@ -6,7 +7,8 @@ class ImportData():
 
     # Read csv data
     def read_csv(self):
-        df = pd.read_csv(self.path)
+        df = pd.read_csv(self.path, error_bad_lines=False, engine='python')
+        df = df.rename(columns={'content':'text', 'Body':'text', 'Fake':'label'})[['text', 'label']]
         return df
 
     # Clean dataframe
@@ -18,10 +20,17 @@ class ImportData():
         print('Null rows removed:', len(df)-len(df_no_na))
         
         # Drop duplicates
-        df_no_duplicates = df_no_na.drop_duplicates(subset='Text')
-        print('Duplicates removed:', len(df)-len(df_no_duplicates))
+        df_no_duplicates = df_no_na.drop_duplicates(subset='text')
+        print('Duplicates removed:', len(df_no_na)-len(df_no_duplicates))
 
-        return df_no_duplicates
+        # Only relevant target varibales
+        df['label'] = df['label'].astype(str)
+        valid_labels = df['label'].value_counts(normalize=True)[df['label'].value_counts(normalize=True)>0.05].index[:1]
+        df_valid_labels = df_no_duplicates[df['label'].isin(valid_labels)]
+        # df_valid_labels = df_no_duplicates[df_no_duplicates['label'].isin(['0','1'])]
+        print('Bad Labels removed:', len(df_no_duplicates)-len(df_valid_labels))
+
+        return df_valid_labels
 
     # Print pandas properties
     def print_pstats(self):
@@ -34,4 +43,7 @@ class ImportData():
         return df
 
 if __name__ == "__main__":
-    ImportData(path="data\\raw\\mock_train.csv").print_pstats()
+    ImportData(path="data\\raw\\translated_dataset_all.csv").print_pstats()
+    # data\raw\translated_test_5k_english.csv
+    # data\\raw\\NELA_20_subset_200k.csv
+
